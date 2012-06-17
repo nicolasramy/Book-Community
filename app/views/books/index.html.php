39a0d1ccf6fe4f93c71cf23f158c644f75d0e88d
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-17 20:12:47 +0200

    Design Books views

--- app/views/books/index.html.php
+++ app/views/books/index.html.php
@@ -1,11 +1,11 @@
 <h2>Books</h2>
 
 <?php foreach ($books as $book) : ?>
-<h3><?php echo $this->html->link($book->name, array('Books::view', 'id' => $book->id)) ?></h3>
-<h4>From <?php $this->html->link($book->author_id, array('Authors::view', 'id' => $book->author_id)) ?></h4>
+<h3><?php echo $this->html->link($book->name, array('Books::view', 'id' => $book->id)); ?></h3>
+<h4>From <?php echo $this->html->link($book->author->firstname . ' ' . $book->author->lastname, array('Authors::view', 'id' => $book->author->id)) ?></h4>
 <p>
 	<?php echo $book->description; ?><br />
+	Published <?php echo $book->published?><br />
 	<?php echo $this->html->link('Read more ...', array('Books::view', 'id' => $book->id)); ?>
 </p>
-<p><?php echo $book->description; ?> <?php echo $book->description; ?></p>
 <?php endforeach; ?>
--- app/views/books/view.html.php
+++ app/views/books/view.html.php
@@ -1,1 +1,10 @@
+<h2>Books</h2>
+
+<h3><?php echo $book->name; ?></h3>
+<h4>From <?php echo $this->html->link($book->author->firstname . ' ' . $book->author->lastname, array('Authors::view', 'id' => $book->author->id)) ?></h4>
+<p>
+	<?php echo $book->description; ?><br />
+	Published <?php echo $book->published?>
+</p>
+
 
