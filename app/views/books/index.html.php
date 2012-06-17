6e8172191c6337da98597ee3d9e45ef77c6b61fb
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-17 19:25:32 +0200

    Designing Books index view

--- app/views/books/index.html.php
+++ app/views/books/index.html.php
@@ -1,1 +1,11 @@
-Hello World!
+<h2>Books</h2>
+
+<?php foreach ($books as $book) : ?>
+<h3><?php echo $this->html->link($book->name, array('Books::view', 'id' => $book->id)) ?></h3>
+<h4>From <?php $this->html->link($book->author_id, array('Authors::view', 'id' => $book->author_id)) ?></h4>
+<p>
+	<?php echo $book->description; ?><br />
+	<?php echo $this->html->link('Read more ...', array('Books::view', 'id' => $book->id)); ?>
+</p>
+<p><?php echo $book->description; ?> <?php echo $book->description; ?></p>
+<?php endforeach; ?>
