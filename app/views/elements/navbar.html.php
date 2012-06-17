f8b8036f762f977473e1fd2fa4e5c4b8e43f056a
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-17 22:13:23 +0200

    Update default layout

---
+++ app/views/elements/navbar.html.php
@@ -0,0 +1,33 @@
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+
+			<a class="brand" href="/">Book Community</a>
+
+			<div class="nav-collapse">
+			<ul class="nav">
+				<li>
+					<?php echo $this->html->link('Books', array('Books::index')); ?>
+				</li>
+				<li>
+					<?php echo $this->html->link('Authors', array('Authors::index')); ?>
+				</li>
+			</ul>
+
+			<ul class="nav pull-right">
+				<li>
+					<?php echo $this->html->link('Login', array('Authors::login')); ?>
+				</li>
+				<li>
+					<?php echo $this->html->link('Register', array('Authors::add')); ?>
+				</li>
+			</ul>
+			</div>
+		</div>
+	</div>
+</div>
--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -10,27 +10,21 @@
 <html>
 <head>
 	<?php echo $this->html->charset();?>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Book Community &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('bootstrap.min', 'bootstrap-responsive.min', 'styles')); ?>
-	<?php //echo $this->scripts('jquery-1.7.2.min', 'bootstrap.min', 'jquery.ui.totop', 'bootstrap-alert'); ?>
-	<?php echo $this->scripts(); ?>
+	<?php echo $this->html->style(array('bootstrap.min', 'bootstrap-responsive.min', 'style')); ?>
+	<?php echo $this->html->script(array('jquery-1.7.2.min', 'bootstrap.min')); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body id="top">
-	<div id="container">
-		<header>
-			<h1>Book Community</h1>
-			<ul>
-				<li><?php echo $this->html->link('Books', array('Books::index')); ?></li>
-				<li><?php echo $this->html->link('Authors', array('Authors::index')); ?></li>
-			</ul>
-		</header>
-		<div id="content">
-			<?php echo $this->content(); ?>
-		</div>
-		<footer>
-			Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>
-		</footer>
+
+	<?php echo $this->_render('element', 'navbar'); ?>
+
+	<div class="container">
+		<?php echo $this->content(); ?>
 	</div>
+
+	<?php echo $this->html->script(array('jquery.ui.totop', 'bootstrap-alert')); ?>
+
 </body>
 </html>
