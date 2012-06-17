fec9dbad3e5654f4df3b34129ce918792c316063
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-17 20:28:41 +0200

    Create default view View

--- app/views/authors/view.html.php
+++ app/views/authors/view.html.php
@@ -1,1 +1,21 @@
+<h2>Authors</h2>
+
+<h3><?php echo $author->firstname; ?>&nbsp;<?php echo $author->lastname; ?></h3>
+<dl>
+	<dt>Email :</dt>
+	<dd><?php echo $author->email; ?></dd>
+	<dt>Date of Birth :</dt>
+	<dd><?php echo $author->date_of_birth; ?></dd>
+</dl>
+<h4>Books</h4>
+<?php if (!empty($author->books)) : ?>
+	<ul>
+	<?php foreach ($author->books as $book) : ?>
+		<li><?php echo $this->html->link($book->name, array('Books::view', 'id' => $book->id)); ?></li>
+	<?php endforeach; ?>
+	</ul>
+<?php else : ?>
+	<p>No books available</p>
+<?php endif; ?>
+
 
