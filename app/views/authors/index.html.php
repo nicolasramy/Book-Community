6b456ff87779574f38f515779bf83bea24b7d168
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-17 20:27:52 +0200

    Update Authors index

--- app/views/authors/index.html.php
+++ app/views/authors/index.html.php
@@ -1,1 +1,6 @@
-Hello World!
+<h2>Authors</h2>
+
+<?php foreach ($authors as $author) : ?>
+<h3><?php echo $this->html->link($author->firstname . '&nbsp;'. $author->lastname, array('authors::view', 'id' => $author->id)) ?></h3>
+
+<?php endforeach; ?>
